ff2e44786e0b4cbaa25cd68b2c8c893266e58abd
Axel Maldonado <sonic775@outlook.es>
2023-01-24 02:16:49 -0300

    add range input for amount of cells

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+const value = document.querySelector("#grid_size_value");
+const input = document.querySelector("#grid_size");
+const grid = document.querySelector('.grid');
+const color_picker = document.querySelector('#color_picker');
+const reset_button = document.querySelector('#reset');
+
+let current_color = 'black';
+let cells_amount = 256;
+let cell_size = 0;
 let mouseDown = false;
+
+calculateCellSize();
+createGrid();
+
+value.textContent = input.value + 'x' + input.value;
+
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value + 'x' + event.target.value;
+    cells_amount = event.target.value*event.target.value;
+    cleanGrid();
+    calculateCellSize();
+    createGrid();
+});
+color_picker.oninput = (e) => current_color = e.target.value;
+reset_button.addEventListener('click', () => grid.childNodes.forEach( (cell) => {cell.style.backgroundColor = 'white';}));
 document.body.addEventListener('mousedown', (e) => {
     if(e.button === 0){
     mouseDown = true;
@@ -8,25 +32,19 @@ document.body.addEventListener('mouseup', (e) => {
     mouseDown = false;
 }});
 
-const grid_size = '360px';
-const grid = document.querySelector('.grid');
-grid.style.width = grid_size;
-grid.style.height = grid_size;
-
-const cells_amount = 16*16;
-const cell_size = Math.sqrt(360*360/cells_amount);
-
-const color_picker = document.querySelector('#color_picker');
-color_picker.oninput = (e) => current_color = e.target.value;
-let current_color = 'black';
-
-const reset_button = document.querySelector('#reset');
-reset_button.addEventListener('click', () => grid.childNodes.forEach( (cell) => {cell.style.backgroundColor = 'white';}));
-
-for (let i = 0; i < cells_amount; i++) {
-    grid.appendChild(createCell(cell_size));
+function cleanGrid(){
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
 }
-
+function createGrid() {
+    for (let i = 0; i < cells_amount; i++) {
+        grid.appendChild(createCell(cell_size));
+    }    
+}
+function calculateCellSize() {
+    cell_size = Math.sqrt(360*360/cells_amount)
+}
 function createCell(cell_size) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -37,10 +55,7 @@ function createCell(cell_size) {
     cell.addEventListener('mousedown', paintCell);
     return cell;
 }
-
 function paintCell(e){
     if (e.type === 'mouseover' && !mouseDown) return
     e.target.style.backgroundColor = current_color;
 }
-
-
