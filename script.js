233990109289dae9bdf19853a53822b176b20c95
Axel Maldonado <sonic775@outlook.es>
2023-01-25 16:47:05 -0300

    add rainbow button

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const input = document.querySelector("#grid_size");
 const grid = document.querySelector('.grid');
 const color_picker = document.querySelector('#color_picker');
 const reset_button = document.querySelector('#reset');
+const rainbow_button = document.querySelector('#rainbow');
 const grid_size = 800;
 
+let rainbow_mode = false;
 let current_color = 'black';
 let cells_amount = 256;
 let cell_size = 0;
@@ -18,19 +20,22 @@ calculateCellSize();
 createGrid();
 current_color = color_picker.childNodes[1].value; 
 
-input.addEventListener("input", (event) => {
+color_picker.oninput = (e) => current_color = e.target.value;
+rainbow_button.addEventListener('click', () => rainbow_mode = !rainbow_mode)
+input.addEventListener('input', (event) => {
     value.textContent = event.target.value + 'x' + event.target.value;
     cells_amount = event.target.value*event.target.value;
     cleanGrid();
     calculateCellSize();
     createGrid();
 });
-color_picker.oninput = (e) => current_color = e.target.value;
+
 reset_button.addEventListener('click', () => grid.childNodes.forEach( (cell) => {cell.style.backgroundColor = 'white';}));
 document.body.addEventListener('mousedown', (e) => {
     if(e.button === 0){
     mouseDown = true;
 }});
+
 document.body.addEventListener('mouseup', (e) => {
     if(e.button === 0){
     mouseDown = false;
@@ -41,14 +46,17 @@ function cleanGrid(){
         grid.removeChild(grid.firstChild);
     }
 }
+
 function createGrid() {
     for (let i = 0; i < cells_amount; i++) {
         grid.appendChild(createCell(cell_size));
     }    
 }
+
 function calculateCellSize() {
     cell_size = Math.sqrt(grid_size*grid_size/cells_amount)
 }
+
 function createCell(cell_size) {
     const cell = document.createElement('div');
     cell.classList.add('cell');
@@ -59,7 +67,22 @@ function createCell(cell_size) {
     cell.addEventListener('mousedown', paintCell);
     return cell;
 }
+
 function paintCell(e){
     if (e.type === 'mouseover' && !mouseDown) return
-    e.target.style.backgroundColor = current_color;
+    if(rainbow_mode)
+    {
+        e.target.style.backgroundColor = randomColor();
+    }else{
+        e.target.style.backgroundColor = current_color;
+    }
+}
+
+function randomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
